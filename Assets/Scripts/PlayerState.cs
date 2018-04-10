6e7ad63c8f6ca1f72f08964291cb49e630d8cdf0
kimgaejin <kimgaejin@naver.com>
2018-04-10 21:58:13 +0900

    키보드입력 및 플레이어 점프/ 조이스틱은 꺼놨음
    a, d 키로 좌우 전환
    j 키로 점프 가능. 점프 약 110px
    조이스틱기능은 꺼놧음.,
    지면의 위에서만 점프 가능

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -8,13 +8,21 @@ public class PlayerState : MonoBehaviour {
     private GameObject keypadCanvas;
     private Joystick joystick;
     private Rigidbody2D rigid;
+    private SpriteRenderer spr;
 
     // setting
     private float horizonSpeed = 0.05f;
+	private float jumpPower = 5.0f;
+
+    // save
+    private Vector3 fixedPoint = Vector3.zero;
 
     // bit switch
-    private bool isFix;
+    private bool isFixed;
     private bool canMove;
+    private bool isJumping;
+    private bool canJump;
+
 
     void Awake()
     {
@@ -25,34 +33,87 @@ public class PlayerState : MonoBehaviour {
     
     void Update()
     {
-        if (isFix == false) { rigid.velocity = Vector2.zero; }
-        else { 
-            Move();
+        Move();
+        Jump();
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("충돌중");
+        if (other.tag == "ground")
+        {
+            float otherSizeX = other.bounds.size.x;
+            float otherSizeY = other.bounds.size.y;
+            float distanceX = this.transform.position.x - other.transform.position.x;
+            float distanceY = this.transform.position.y - other.transform.position.y;
+
+            distanceX = Mathf.Abs(distanceX);
+
+            if (distanceX < otherSizeX/2 + spr.bounds.size.x/2 && distanceY >= otherSizeY/2)
+            {
+                Debug.Log("distanceX " + distanceX);
+                Debug.Log("distanceY " + distanceY);
+                if (rigid.velocity.y <= 0)
+                {
+                    isJumping = false;
+                }
+            }
         }
-        
-        
-        
     }
 
     private void Move()
     {
+        if (isFixed == true) {
+            rigid.velocity = Vector2.zero;
+            rigid.position = fixedPoint;
+            return;
+        }
         if (canMove == false) return;
 
         float horizon;
-        horizon = joystick.GetHorizontalValue();
+       
+        //horizon = joystick.GetHorizontalValue();  // 조이스틱용
+
+        // 키보드용
+        if (Input.GetKey(KeyCode.A)) horizon = -1.0f;
+        else if (Input.GetKey(KeyCode.D)) horizon = 1.0f;
+        else  horizon = 0;
+
         this.transform.position += horizon * horizonSpeed * Vector3.right;
 
+        fixedPoint = this.transform.position;
+    }
+
+    private void Jump()
+    {
+        if (isJumping == true) return;
+        if (canJump == false) return;
+
+        //bool isTouchingJump = false;
+        //if (GetStateJumpImg() == true)    // 터치패널용
+        // 키보드용
+        if (Input.GetKey(KeyCode.J))
+        {
+            Debug.Log("쩜프!");
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+		
+		isJumping = true;
     }
 
     private void InitializeBitSwitch()
     {
-        isFix = false;
+        isFixed = false;
         canMove = true;
+        canJump = true;
+        isJumping = true;
     }
 
     private void InitializeComponent()
     {
         rigid = this.gameObject.GetComponent<Rigidbody2D>();
+        spr = this.transform.FindChild("pGraphic").GetComponent<SpriteRenderer>();
     }
 
     private void InitializeKeypad()
