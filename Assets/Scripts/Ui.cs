7cb92ee3b951be9c2257bb9e056baa9b062344c9
agw5256 <agw5256@naver.com>
2018-05-07 15:18:08 +0900

    버튼 함수들 추가

---
+++ Assets/Scripts/Ui.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ui : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Start_Game() {
+
+
+    }
+
+    public void Exit_Game()
+    {
+
+
+    }
+
+    public void Diary()
+    {
+
+
+    }
+
+
+    public void Set_Up()
+    {
+
+
+    }
+
+    public void Exit_Set_Up()
+    {
+
+
+    }
+
+    public void Diary_Chapter()
+    {
+
+
+    }
+
+    public void Exit_Portal()
+    {
+
+
+    }
+
+    public void Enter()
+    {
+
+
+    }
+
+    public void Progression_Exit()
+    {
+
+
+    }
+}
