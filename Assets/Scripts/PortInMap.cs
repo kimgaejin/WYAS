95c03df04eb6a83190ca3fffa1cac456543e87b6
kimgaejin <kimgaejin@naver.com>
2018-05-20 23:32:26 +0900

    오브젝트구현(포탈)
    * 포탈 구현 완료.
    * 너무 오래 쉬었네요 죄송합니다.
    * 포탈 작동대기시간(?)이 없어서 와리가리 막 계속 누르면 포탈과 포탈 사이에서 카메라가 와리가리하지만 그 외엔 별로 상관없습니다. 서둘러 카메라가 부자연스러운거나 고쳐야겠습니다.

---
+++ Assets/Scripts/PortInMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortInMap : ObjectProperty
+{
+    private Transform linkLocation;
+
+    private void Start()
+    {
+        base.rangeX = 0.3f;
+        base.mustFaced = false;
+
+        FindLinkedPortal();
+    }
+
+    override public void DoInteracting()
+    {
+        pState.transform.position = linkLocation.position;
+        StopInteracting();
+    }
+
+    override public void IsInteracting()
+    {
+
+    }
+
+    override public void StopInteracting()
+    {
+
+    }
+
+    void FindLinkedPortal()
+    {
+        linkLocation = transform.parent.transform.GetChild(0);
+        if (linkLocation == this.transform) linkLocation = transform.parent.transform.GetChild(1);
+    }
+}
---
+++ Assets/Scripts/PortalInMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalInMap : ObjectProperty
+{
+    private Transform location;
+
+    private void Start()
+    {
+        base.rangeX = 0.3f;
+        base.mustFaced = false;
+
+        location = transform.GetChild(0);
+    }
+
+    override public void DoInteracting()
+    {
+
+    }
+
+    override public void IsInteracting()
+    {
+        
+    }
+
+    override public void StopInteracting()
+    {
+      
+    }
+
+}
